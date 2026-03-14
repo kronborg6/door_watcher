b54e994855979912fd4f0e43a7dc808621290415
Kronborg <mkronborg7@gmail.com>
2026-03-14 13:01:24 +0100

    i made somfing blink

--- main/door_watcher.c
+++ main/door_watcher.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/projdefs.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "hal/gpio_types.h"
+
+#define LED_GPIO 2
 
 void app_main(void)
 {
+    gpio_config_t io_conf = {
+        .pin_bit_mask = 1ULL << LED_GPIO,
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
+
+    gpio_config(&io_conf);
+    for (;;) {
+    gpio_set_level(LED_GPIO, 1);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    gpio_set_level(LED_GPIO, 0);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
 
 }
